9ae1beb21f7317a7f386f52de01ca5093d8fad3c
KentMG <4gunns.k@gmail.com>
2019-04-11 11:10:04 -0500

    creating post upload functions

---
+++ functions/UploadPost.js
@@ -0,0 +1,66 @@
+import RNFetchBlob from 'react-native-fetch-blob';
+import { db, storage } from '../config'
+
+const Blob = RNFetchBlob.polyfill.Blob
+const fs = RNFetchBlob.fs
+window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
+window.Blob = Blob
+
+
+export const uploadPost = (uri, postData, mime = 'application/octet-stream') => {
+    return(dispatch => {
+        return new Promise((resolve, reject) => {
+            const uploadUri = Platform.OS === 'ios' ? uri.replace('file://','') : uri
+            const sessionId = new Date().getTime()
+            let uploadBlob = null
+
+            const imageRef = storage.ref('photos').child(uri)
+
+            fs.readFile(uploadUri, 'base64')
+            .then((data) => {
+                return Blob.build(data, {type: `${mime};BASE64`})
+            })
+            .then((blob) => {
+                uploadBlob = blob
+                return imageRef.put(blob, { contentType: mime })
+            })
+            .then(() => {
+                uploadBlob.close()
+                return imageRef.getDownloadURL()
+            })
+            .then((url) => {
+                resolve(url)
+                storeReference(url, sessionId, postData)
+            })
+            .catch((error) => {
+                reject(error)
+            })
+        })
+    })
+}
+
+const storeReference=(downloadUrl, sessionId, postData) => {
+    let imageRef = storage.ref('photos').child(uri)
+    let currentUser = firebase.auth.currentUser
+    let image = {
+        type: 'image',
+        url: downloadUrl,
+        createdAt: sessionId,
+        user: {
+            id: currentUser.uid,
+            email: currentUser.email
+        }
+    }
+    let post = {
+        Image: imageRef,
+        Imageobj: image,
+        Status: Pending,
+        Imgcomplete:"",
+        Imgcompleteobj:"",
+        Description:"",
+        Location:"",
+        TimeSpent:"",
+        AfterDescription:""
+    }
+    db.ref('posts').push(post)
+}
