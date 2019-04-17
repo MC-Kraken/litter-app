11a603e4bbf612a2aa7e67260f91ebf43781671e
JamesM23 <jhmay30@gmail.com>
2019-04-17 10:21:53 -0500

    lead

--- screens/Leaderboard.js
+++ screens/Leaderboard.js
@@ -9,20 +9,41 @@ export default class Leaderboard extends Component {
         <Content>
           <List>
             <ListItem avatar>
+            <ListItem avatar></ListItem>
+            <ListItem avatar></ListItem>
+            <ListItem avatar></ListItem>
+            <ListItem avatar></ListItem>
               <Left>
+                <Thumbnail source={{ uri: 'Image URL' }} />
+                <Thumbnail source={{ uri: 'Image URL' }} />
+                <Thumbnail source={{ uri: 'Image URL' }} />
+                <Thumbnail source={{ uri: 'Image URL' }} /> 
                 <Thumbnail source={{ uri: 'Image URL' }} />
               </Left>
               <Body>
-                <Text>Kumar Pratik</Text>
+                <Text>Jumar Johnson</Text>
+                <Text>Mike Terry</Text>
+                <Text>Lucy Moore</Text>
+                <Text>Lumar Perry</Text>
+                <Text>John Williams</Text>
+                <Text note>Doing what you like will always keep you happy . .</Text>
+                <Text note>Doing what you like will always keep you happy . .</Text>
+                <Text note>Doing what you like will always keep you happy . .</Text>
+                <Text note>Doing what you like will always keep you happy . .</Text>
                 <Text note>Doing what you like will always keep you happy . .</Text>
               </Body>
               <Right>
-                <Text note>3:43 pm</Text>
+                <Text note>3:46 pm</Text>
+                <Text note>5:00 pm</Text>
+                <Text note>7:03 pm</Text>
+                <Text note>12:13 pm</Text>
+                <Text note>10:41 pm</Text>
               </Right>
             </ListItem>
           </List>
         </Content>
       </Container>
+      
     );
   }
 }
